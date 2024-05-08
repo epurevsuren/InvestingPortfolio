c9619a04697e405940d8c3712dee8eb318724f40
Purevsuren Erdene <purevsuren.er@gmail.com>
2024-05-09 01:34:48 +1000

    Initial Commit

---
+++ InvestingPortfolio/InvestingPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestingPortfolioApp.swift
+//  InvestingPortfolio
+//
+//  Created by Purevsuren Erdene on 9/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct InvestingPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
